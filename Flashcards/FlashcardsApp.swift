dc1ad0d1fe45e6069ade64671a534b987b0427bf
Adam Iskounen <adam.iskounen@artsymail.com>
2021-01-22 19:56:50 -0500

    Initial Commit

---
+++ Flashcards/FlashcardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashcardsApp.swift
+//  Flashcards
+//
+//  Created by Adam Iskounen on 1/22/21.
+//
+
+import SwiftUI
+
+@main
+struct FlashcardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
